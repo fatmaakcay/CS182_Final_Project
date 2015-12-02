7ad5b0dafa095ac67dd5928c6774411e83317243
Khalid Tawil <tawilk2014@gmail.com>
2015-12-01 21:18:51 -0500

    I don't want you to leave, will you hold my hand?
    Created a function that takes in an image file and outputs a 2D binary
    array that represents the image.

---
+++ image_pro/data.py
@@ -0,0 +1,47 @@
+###############################################################################
+# COMPSCI 182
+# November 2015
+# Khalid Tawil
+#
+# Takes in an image and reduces it to a 40x40 black and white image
+# To see an image use im.show()
+# USAGE: binary_image("colorfulsmiley.png")
+###############################################################################
+from PIL import Image
+
+# Dimensions of our PNGs
+size = 40, 40
+
+def binary_image(file):
+    # Loading file into memory
+    im = Image.open(file)
+
+    # Resizes the image
+    im = im.resize(size, Image.ANTIALIAS)
+
+    # Grayscales the image
+    threshold = 250
+    im = im.point(lambda p: p > threshold and 255)
+    im = im.convert('1')
+    im.show()
+
+    # Converting our image into a 2-dimensional array of binary values
+    data = list(im.getdata())
+    binary_data =[]
+    for i in range(size[0]):
+        row = data[ (i * size[0]) : ((i+1) * size[0]) ]
+        row = [1 if x==255 else x for x in row]
+        binary_data.append(row)
+
+    return binary_data
+
+result = binary_image("Images/colorfulsmiley.png")
+
+# This just pretty prints
+s = [[str(e) for e in row] for row in result]
+lens = [max(map(len, col)) for col in zip(*s)]
+fmt = ' '.join('{{:{}}}'.format(x) for x in lens)
+table = [fmt.format(*row) for row in s]
+print '\n'.join(table)
+
+print "Output finished"
