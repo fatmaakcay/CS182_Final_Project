388360ddd895fd921c8d0f6fcbdbb88673b939c3
thctamm <tamm@college.harvard.edu>
2015-12-03 19:29:04 -0500

    added functionality for testing

--- main.py
+++ main.py
@@ -33,3 +33,26 @@ def backprop_train(test_len):
 def genetic_train():
     net = nn.init_net()
     return net
+
+
+def training_results(train_len, type="BP"):
+
+    if type == "BP" or type == "GEN":
+        if type == "BP":
+            net = backprop_train(train_len)
+        else:
+            net = genetic_train()
+        for emotion in emojis:
+            test_cases = glob.glob('./test_data/' + emotion + '/*.png')
+            inp = random.choice(test_cases)
+            result = net.forward_pass(inp)
+            print(emotion + ":")
+            print("     hearth eyes:" + str(result[0]*100) + "%")
+            print("     laugh:" + str(result[1]*100) + "%")
+            print("     sad:" + str(result[2]*100) + "%")
+            print("     smile:" + str(result[3]*100) + "%")
+    else:
+        print("ERROR: Choose correct training type: BP - Backpropagation, GEN - genetic algorithm")
+
+
+training_results(1000, "BP")
