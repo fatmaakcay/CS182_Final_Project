4a3fd55ca710fcec27f6ce02b88750584c7da81d
Khalid Tawil <tawilk2014@gmail.com>
2015-12-05 15:37:29 -0500

    ImageConverter now creates 4 separate files
    We do not yet have 45 of each image. Coming very soon.

--- imageConverter.py
+++ imageConverter.py
@@ -9,15 +9,14 @@ import csv
 emojis = ["hearts", "laugh", "sad", "smile"]
 
 # CSV Writer
-f = open('binarytext.csv', 'w')
-w = csv.writer(f)
 
 for i in range(4):
+    f = open(emojis[i]+'text.csv', 'w')
+    w = csv.writer(f)
     test_cases = glob.glob('./test_data/' + emojis[i] + '/*.png')
     print 'Loop ' + str(i)
     for test_case in test_cases:
         data = image.binary_image(test_case, cfg.RES)
         data = image.convert_to_1d(data)
         w.writerow(data)
-
-f.close()
+    f.close()
